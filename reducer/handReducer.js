5d358863057f8286713e5efcfa6290913dba233d
Drake Bott <bott.da@gmail.com>
2019-02-04 21:46:01 -0700

    Create hand redcuer

---
+++ reducer/handReducer.js
@@ -0,0 +1,30 @@
+import { actionTypes } from '../actions';
+
+const initialState = [];
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case actionTypes.addCardToHand:
+      return [...state, action.card];
+    case actionTypes.discardCard:
+      return state.filter(card => card.id === action.card.id);
+    case actionTypes.discardHand:
+      return initialState;
+    case actionTypes.hideCard:
+      return state.map(card => {
+        if (card.id === action.card.id) {
+          return { ...card, hidden: true };
+        }
+        return card;
+      });
+    case actionTypes.revealCard:
+      return state.map(card => {
+        if (card.id === action.card.id) {
+          return { ...card, hidden: false };
+        }
+        return card;
+      });
+    default:
+      return state;
+  }
+}
