52cf83cf857f7dba4f443526d0173f0ca3327e7d
Drake Bott <bott.da@gmail.com>
2019-02-04 21:57:59 -0700

    Import all reducers and combine into root reducer

--- reducer/index.js
+++ reducer/index.js
@@ -1,19 +1,16 @@
-import { actionTypes } from '../actions';
+import { combineReducers } from 'redux';
+import connectionStatusReducer from './connectionStatusReducer';
+import handReducer from './handReducer';
+import ownerReducer from './ownerReducer';
+import roomIdReducer from './roomIdReducer';
+import userIdReducer from './userIdReducer';
+import usernameReducer from './usernameReducer';
 
-export const initialState = {
-  connected: false,
-  roomID: '',
-  username: '',
-  userID: null,
-  owner: false,
-  hand: [],
-};
-
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    default:
-      return state;
-  }
-}
-
-export default reducer;
+export default combineReducers({
+  connected: connectionStatusReducer,
+  hand: handReducer,
+  owner: ownerReducer,
+  roomID: roomIdReducer,
+  userID: userIdReducer,
+  username: usernameReducer,
+});
