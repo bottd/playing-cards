fb448001ad6e190ba31e1da9c280ed4268c7ff18
Drake Bott <bott.da@gmail.com>
2019-02-04 20:54:11 -0700

    Strip out boilerplate redux store

--- reducer/index.js
+++ reducer/index.js
@@ -1,52 +1,10 @@
 import { actionTypes } from '../actions';
 
-export const exampleInitialState = {
-  count: 0,
-  error: false,
-  lastUpdate: 0,
-  light: false,
-  placeholderData: null,
-  socket: null,
+export const initialState = {
 };
 
-function reducer(state = exampleInitialState, action) {
+function reducer(state = initialState, action) {
   switch (action.type) {
-    case actionTypes.FAILURE:
-      return {
-        ...state,
-        ...{ error: action.error },
-      };
-
-    case actionTypes.INCREMENT:
-      return {
-        ...state,
-        ...{ count: state.count + 1 },
-      };
-
-    case actionTypes.DECREMENT:
-      return {
-        ...state,
-        ...{ count: state.count - 1 },
-      };
-
-    case actionTypes.RESET:
-      return {
-        ...state,
-        ...{ count: exampleInitialState.count },
-      };
-
-    case actionTypes.LOAD_DATA_SUCCESS:
-      return {
-        ...state,
-        ...{ placeholderData: action.data },
-      };
-
-    case actionTypes.TICK_CLOCK:
-      return {
-        ...state,
-        ...{ lastUpdate: action.ts, light: !!action.light },
-      };
-
     default:
       return state;
   }
