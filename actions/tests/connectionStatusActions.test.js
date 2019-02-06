d2603c83d8ff51b97ad71ac39c8af6c93819a304
Drake Bott <bott.da@gmail.com>
2019-02-05 18:49:10 -0700

    Add it blocks to test file

--- actions/tests/connectionStatusActions.test.js
+++ actions/tests/connectionStatusActions.test.js
@@ -8,14 +8,18 @@ describe('Connection status actions', () => {
   });
 
   describe('connectToGame', () => {
-    const expected = { type: 'CONNECT_TO_GAME' };
-    const result = connectToGame();
-    expect(result).toEqual(expected);
+    it('Should return an action with correct type', () => {
+      const expected = { type: 'CONNECT_TO_GAME' };
+      const result = connectToGame();
+      expect(result).toEqual(expected);
+    });
   });
 
   describe('disconnectFromGame', () => {
-    const expected = { type: 'DISCONNECT_FROM_GAME' };
-    const result = disconnectFromGame();
-    expect(result).toEqual(expected);
+    it('Should return an action with correct type', () => {
+      const expected = { type: 'DISCONNECT_FROM_GAME' };
+      const result = disconnectFromGame();
+      expect(result).toEqual(expected);
+    });
   });
 });
