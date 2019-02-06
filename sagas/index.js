4b259b7dc89f12d3a12dc687b5e74dea39470e9b
Drake Bott <bott.da@gmail.com>
2019-02-06 14:08:23 -0700

    Connect socket in saga

--- sagas/index.js
+++ sagas/index.js
@@ -1,10 +1,18 @@
 import { all, delay, call, put, take, takeLatest } from 'redux-saga/effects';
 import es6promise from 'es6-promise';
 import 'isomorphic-unfetch';
+import io from 'socket.io-client'
+import { setSocket } from '../actions/socketActions';
 
 es6promise.polyfill();
 
+function connectToServer() {
+  return io();
+}
+
 function* rootSaga() {
+  const socket = yield connectToServer();
+  yield put(setSocket(socket));
   yield all([
   ]);
 }
