3473ed3f4f97d437fbeea8ff48776eb69e266163
Drake Bott <bott.da@gmail.com>
2019-02-05 18:53:12 -0700

    Create roomIdActions tests

---
+++ actions/tests/roomIdActions.test.js
@@ -0,0 +1,29 @@
+import { actionTypes, resetRoomID, updateRoomID } from '../roomIdActions';
+
+describe('roomIdActions', () => {
+  describe('action types', () => {
+    it('Should match snapshot', () => {
+      expect(actionTypes).toMatchSnapshot();
+    });
+  });
+
+  describe('resetRoomID', () => {
+    it('Should return an action with correct type', () => {
+      const expected = { type: 'RESET_ROOM_ID' };
+      const result = resetRoomID();
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('updateRoomID', () => {
+    it('Should return an action with correct type', () => {
+      const expected = 'UPDATE_ROOM_ID';
+      const result = updateRoomID(1);
+      expect(result.type).toBe(expected);
+    });
+    it('Should match snapshot', () => {
+      const result = updateRoomID(1);
+      expect(result).toMatchSnapshot();
+    });
+  });
+});
