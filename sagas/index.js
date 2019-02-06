06e8a3ea1696acc8d2e21ee44edfad4c1dbd483c
Drake Bott <bott.da@gmail.com>
2019-02-06 14:30:44 -0700

    Add saga to take when user requests to connect

--- sagas/index.js
+++ sagas/index.js
@@ -2,17 +2,17 @@ import { all, delay, call, put, take, takeLatest } from 'redux-saga/effects';
 import es6promise from 'es6-promise';
 import 'isomorphic-unfetch';
 import io from 'socket.io-client'
+import actionTypes from '../actions/actionTypes';
 import { setSocket } from '../actions/socketActions';
 
 es6promise.polyfill();
 
-function connectToServer() {
-  return io();
+function* connectToServer() {
+  const socket = yield io();
+  yield put(setSocket(socket));
 }
 
 function* rootSaga() {
-  const socket = yield connectToServer();
-  yield put(setSocket(socket));
   yield all([
   ]);
 }
