ef66b3cbb495965a8ce25c0e5ac437ddb7c303d9
Drake Bott <bott.da@gmail.com>
2019-02-07 00:24:26 -0700

    Style connect form

--- components/ConnectForm/index.js
+++ components/ConnectForm/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './ConnectForm.css';
 
 export default function ConnectForm(props) {
   const [name, setName] = useState('');
@@ -32,16 +31,73 @@ export default function ConnectForm(props) {
       </div>
       {selected === 'player' ? (
         <form className="player-form">
-          <input value={name} onChange={e => handleChange(e, setName)} />
-          <input value={gameId} onChange={e => handleChange(e, setGameId)} />
-          <button type="submit" onClick={handleSubmit} />
+          <input
+            placeholder="Player Name"
+            value={name}
+            onChange={e => handleChange(e, setName)}
+          />
+          <input
+            placeholder="Game ID"
+            value={gameId}
+            onChange={e => handleChange(e, setGameId)}
+          />
+          <button className="join-game" type="submit" onClick={handleSubmit}>
+            Submit
+          </button>
         </form>
       ) : (
         <form className="dealer-form">
-          <input value={name} onChange={e => handleChange(e, setName)} />
-          <button className="create-game">Create Game</button>
+          <input
+            placeholder="Player Name"
+            value={name}
+            onChange={e => handleChange(e, setName)}
+          />
+          <button className="create-game" type="submit">
+            Create Game
+          </button>
         </form>
       )}
+      <style jsx>{`
+        .ConnectForm {
+          background-color: #fff;
+          box-sizing: border-box;
+          height: 40%;
+          width: 60%;
+        }
+        .toggle-container {
+          width: 100%;
+          display: flex;
+          margin-bottom: 1%;
+          height: 15%;
+        }
+        .select-player,
+        .select-dealer {
+          background-color: #fff;
+          border: none;
+          border-bottom: 4px solid transparent;
+          font-size: 1.25em;
+          width: 50%;
+        }
+        .create-game, .join-game {
+          background-color: #fff;
+          border: 1px solid #004489;
+          font-size: 1.25em;
+          width: 31%;
+        }
+        .active {
+          border-bottom: 4px solid #004489;
+        }
+        input {
+          font-size: 1.25em;
+          margin: 0 0 1% 0;
+          width: 30%;
+        }
+        form {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+      `}</style>
     </div>
   );
 }
