f9350ac9b85eca0e088227ae6c9167e3c3fb9f2e
Drake Bott <bott.da@gmail.com>
2019-02-06 19:00:55 -0700

    Remove css files

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import fetch from 'isomorphic-unfetch';
 import { connect } from 'react-redux';
 import { requestSocket } from '../actions/socketActions';
 import ConnectForm from '../components/ConnectForm';
-import './index.css';
 
 class ChatOne extends Component {
   static async getInitialProps({ req }) {
@@ -14,11 +13,23 @@ class ChatOne extends Component {
   render() {
     return (
       <main>
-        <ConnectForm/>
+        <ConnectForm />
+        <style jsx>
+          {`
+            main {
+              background-color: #007328;
+              background-image: url('https://www.transparenttextures.com/patterns/felt.png');
+              background-color: green;
+              height: 100vh;
+              width: 100vw;
+            }
+          `}
+        </style>
       </main>
     );
   }
 }
+
 const mapDispatchToProps = dispatch => ({
   requestSocket: () => dispatch(requestSocket()),
 });
