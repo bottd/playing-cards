bac1610f26cb55c6406fbda826c392df8f877b4b
Drake Bott <bott.da@gmail.com>
2019-02-13 15:09:07 -0700

    Emit game data as a response to connection

--- server.js
+++ server.js
@@ -12,34 +12,38 @@ const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
-const rooms = {};
+const rooms = {
+  ABC: new Game(false, 'John'),
+};
+
+rooms.ABC.dealCard(rooms.ABC.players[0].id);
 
 io.on('connection', socket => {
   console.log('USER CONNECTED');
-  socket.on('joinGame', function({ owner, username, roomID}) {
-    console.log('ARGS', username, owner, roomID);
+  socket.on('joinGame', function({ owner, username, roomID }) {
     socket.username = username;
     socket.owner = owner;
-    if ( owner ) {
+    if (owner) {
       const newRoom = utils.generateRoomID();
-      socket.room = newRoom
       rooms[newRoom] = new Game(false, username);
-      console.log(newRoom);
+      socket.room = newRoom;
+      socket.id = rooms[newRoom].players[0].id;
       socket.join(newRoom);
-      console.log(rooms[newRoom].players);
     } else {
       socket.room = roomID;
-      rooms[roomID].addPlayer(username);
+      socket.id = rooms[roomID].addPlayer(username);
       socket.join(roomID);
-      console.log(rooms[roomID].players);
     }
-  })
+    socket.join(socket.id);
+    const gameData = rooms[socket.room].collectGameData(socket.id);
+    io.to(socket.id).emit('joinGameData', gameData);
+  });
 });
 
 nextApp.prepare().then(() => {
   app.get('*', (req, res) => {
-    return nextHandler(req, res)
-  })
+    return nextHandler(req, res);
+  });
   server.listen(port, err => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
